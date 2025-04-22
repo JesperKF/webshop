f21f566e91616abb59acdb1b53540210ea758fec
Oliver Valter <olva0001@kea.stud.dk>
2025-04-22 17:09:10 +0200

    front page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 import Entry from "./components/Entry";
 
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center bg-[#D21E1F] min-h-screen p-24">
+    <div className="bg-[#ffffff] min-h-screen pl-24 pr-24 pt-10">
       <Entry />
     </div>
   );
