cce4a7d3d7ace312019bd59d23b0cac7adf102a7
Oliver Valter <olva0001@kea.stud.dk>
2025-04-23 23:42:21 +0200

    search field changes added icon

--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from "react";
+import { CiSearch } from "react-icons/ci";
+
 
 const Search = ({ onSearch }) => {
   const [query, setQuery] = useState("");
@@ -13,13 +15,16 @@ const Search = ({ onSearch }) => {
   return (
     <div className="w-[237px] bg-white p-4 border border-gray-200 rounded-lg shadow-md">
       <h1 className="text-black mb-2">Search for a specific item</h1>
+    <div className="relative">
+      <CiSearch className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 scale-150" />
       <input
-        className="rounded-md border-2 border-pink-300 text-black focus:outline-none focus:border-purple-500"
+        className="w-full pr-3 pl-1 py-1 rounded-md border-2 border-pink-300 text-black placeholder-gray-400 focus:placeholder-transparent focus:outline-none focus:border-purple-500"
         type="text"
         value={query}
         onChange={handleChange}
         placeholder="Search..."
-      />
+     />
+      </div>
     </div>
   );
 };
